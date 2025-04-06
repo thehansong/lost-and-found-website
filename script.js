764c612af5f7757f7ec70e96dc4f2143d9e73138
Clarence <clarence1998@hotmail.com>
2025-04-06 21:12:55 +0800

    Revert

--- script.js
+++ script.js
@@ -9,12 +9,6 @@ function renderItemCard(item) {
   card.className = "item-card";
   card.style.textDecoration = "none";
   card.style.color = "inherit";
-  card.style.cursor = "default";
-
-  card.addEventListener("click", function(event) {
-    event.preventDefault();
-  });
-  
 
   const image = document.createElement("img");
   image.src = item.image || "https://via.placeholder.com/150";
