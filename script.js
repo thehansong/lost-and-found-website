c08ba3f6aa98ba496232795e5f8e90f8f616f3d3
Clarence <clarence1998@hotmail.com>
2025-04-06 16:59:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // const API_BASE = "http://localhost:3000"
-const API_BASE = "http://18.214.100.164/"
+const API_BASE = "http://18.214.100.164";
 
 function renderItemCard(item) {
   const itemGrid = document.getElementById("itemGrid");
